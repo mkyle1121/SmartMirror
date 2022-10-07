31604669a3823c8032379dd4268f4a70f89cd9c0
Mike kyle <mkyle1121@hotmail.com>
2022-10-07 07:46:56 -0500

    .

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SmartMirror.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+		public ViewModel()
+		{
+			currentDateTime = DateTime.Now.ToString("H:mm\r\ndddd, MMMM d");
+			GetWeatherAsync();	
+		}
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+		private string currentDateTime;
+
+		public string CurrentDateTime
+		{
+			get { return currentDateTime; }
+			set
+			{ 
+				currentDateTime = value;
+				OnPropertyChanged(nameof(CurrentDateTime));
+			}
+		}
+
+		private WeatherData weatherData;
+
+		public WeatherData WeatherData
+		{
+			get { return weatherData; }
+            set
+            {
+                weatherData = value;
+                OnPropertyChanged(nameof(WeatherData));
+            }
+        }
+
+
+
+		private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+		private async void GetWeatherAsync()
+		{
+            var weatherHelper = new WeatherHelper();
+            weatherData = await weatherHelper.GetWeatherAsync();
+			Console.WriteLine();
+        }
+    }
+}
---
+++ ViewModel/WeatherHelper.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SmartMirror.ViewModel
+{
+    public class WeatherHelper
+    {
+        private static string latitude = "30.266666";
+        private static string longitude = "-97.733330";
+        private static string apiKey = "40421cbe548a05b158f1a3c1c8bb8120";
+        private static string weatherApiEndpoint = $"https://api.openweathermap.org/data/2.5/weather?q=Austin,us&appid={apiKey}";
+
+        public async Task<WeatherData> GetWeatherAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(weatherApiEndpoint);                
+                return JsonConvert.DeserializeObject<WeatherData>(response);
+            }
+        }
+    }
+}
